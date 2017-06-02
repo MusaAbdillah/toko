2abb45a4ac469ab410391384130a943c4f95e1f3
Musa Abdillah <musaabdillahm9@gmail.com>
2017-06-03 03:38:24 +0700

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+	#validation
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: "must be url for gif, jpg, or png"}
 end
