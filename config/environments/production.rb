dd3cabceb3ff0b6986bd1f9f51c90ce27747a3d6
MusaAbdillah <musaabdillahm9@gmail.com>
2018-03-01 00:59:30 +0700

    change setting for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'stark-temple-16554.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'stark-temple-16554.herokuapp.com',
+  :authentication => :plain,
+}
 end
